a3756d83b33cf9921bd68f06c13b3a0ed100118f
zhouzheng <1750931560@qq.com>
2020-09-07 10:41:45 +0800

    first commit

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          SqlConnection conn = new SqlConnection("Data Source=(local);Initial Catalog=yzq;Integrated Security=True");
+          conn.Open();
+            SqlCommand cmd = new SqlCommand("select * from student where sno='" + textBox1.Text.Trim()+"'and postcode='"+textBox2.Text.Trim()+"'", conn);
+            SqlDataReader sdr = cmd.ExecuteReader(); 
+            sdr.Read(); 
+             if (sdr.HasRows)  
+             {
+                 MessageBox.Show("登陆成功", "提示?"); 
+                 Form2 form=new Form2();
+                 form.Show();
+                 this.Hide();
+             }
+             else
+             {
+             MessageBox.Show("提示：学生用户名或密码错误!","警告");
+                 conn.Close();
+             }
+            }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        }
+    }
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form2.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.yzqDataSet = new WindowsFormsApplication1.yzqDataSet();
+            this.cjBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.cjTableAdapter = new WindowsFormsApplication1.yzqDataSetTableAdapters.cjTableAdapter();
+            this.snoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cnoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gradeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yzqDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cjBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.snoDataGridViewTextBoxColumn,
+            this.cnoDataGridViewTextBoxColumn,
+            this.gradeDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.cjBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(126, 113);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(372, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // yzqDataSet
+            // 
+            this.yzqDataSet.DataSetName = "yzqDataSet";
+            this.yzqDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // cjBindingSource
+            // 
+            this.cjBindingSource.DataMember = "cj";
+            this.cjBindingSource.DataSource = this.yzqDataSet;
+            // 
+            // cjTableAdapter
+            // 
+            this.cjTableAdapter.ClearBeforeFill = true;
+            // 
+            // snoDataGridViewTextBoxColumn
+            // 
+            this.snoDataGridViewTextBoxColumn.DataPropertyName = "Sno";
+            this.snoDataGridViewTextBoxColumn.HeaderText = "Sno";
+            this.snoDataGridViewTextBoxColumn.Name = "snoDataGridViewTextBoxColumn";
+            // 
+            // cnoDataGridViewTextBoxColumn
+            // 
+            this.cnoDataGridViewTextBoxColumn.DataPropertyName = "Cno";
+            this.cnoDataGridViewTextBoxColumn.HeaderText = "Cno";
+            this.cnoDataGridViewTextBoxColumn.Name = "cnoDataGridViewTextBoxColumn";
+            // 
+            // gradeDataGridViewTextBoxColumn
+            // 
+            this.gradeDataGridViewTextBoxColumn.DataPropertyName = "Grade";
+            this.gradeDataGridViewTextBoxColumn.HeaderText = "Grade";
+            this.gradeDataGridViewTextBoxColumn.Name = "gradeDataGridViewTextBoxColumn";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 405);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yzqDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cjBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private yzqDataSet yzqDataSet;
+        private System.Windows.Forms.BindingSource cjBindingSource;
+        private yzqDataSetTableAdapters.cjTableAdapter cjTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn snoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cnoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn gradeDataGridViewTextBoxColumn;
+    }
+}
